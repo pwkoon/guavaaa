75d097ac5c0a24540ec16a5ee07497569d2fa224
wankoon puah puahwankoon@gmail.com <puahwankoon@gmail.com>
2023-05-12 23:29:55 +0800

    responsiveeeeeeeeee

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_05_092805) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_07_124059) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,9 +52,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_05_092805) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price_cents", default: 0, null: false
   end
 
   create_table "reviews", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -24,14 +24,14 @@ user = User.create!(
 
 puts "Creating products"
 
-image1 = URI.open("https://unsplash.com/photos/7StiT9uqFyo")
-image2 = URI.open("https://unsplash.com/photos/oPiT4nceNdw")
-image3 = URI.open("https://unsplash.com/photos/dSb8R7QAeLM")
+image1 = URI.open("https://images.unsplash.com/photo-1629367308496-a2496ba22f88?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80")
+image2 = URI.open("https://images.unsplash.com/photo-1629367309270-1e6b8a303630?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")
+image3 = URI.open("https://images.unsplash.com/photo-1571646302210-db7990b30751?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80")
 
 product1= Product.new(
   name: "Guava A",
   description: "Guava trees are native to tropical America and are grown in tropical and subtropical areas worldwide. Guava fruits are processed into jams, jellies, and preserves and are common pastry fillings.",
-  price: "4.50/kg"
+  price_cents: 450
 )
 
 product1.photo.attach(io: image1, filename: "guavaA.jpg", content_type: "image/jpg")
@@ -40,7 +40,7 @@ product1.save!
 product2= Product.new(
   name: "Guava B",
   description: "Looking not so good but taste same as Guava A. Guava trees are native to tropical America and are grown in tropical and subtropical areas worldwide. Guava fruits are processed into jams, jellies, and preserves and are common pastry fillings.",
-  price: "4.00/kg"
+  price: 4
 )
 
 product2.photo.attach(io: image2, filename: "guavaB.jpg", content_type: "image/jpg")
@@ -49,8 +49,10 @@ product2.save!
 product3= Product.new(
   name: "Sekinchan Corn",
   description: "Sweet corn is the result of a naturally occurring recessive mutation in the genes. Which control conversion of sugar to starch inside the endosperm of the corn kernel.",
-  price: "10.00 for 10 pieces"
+  price: 10
 )
 
 product3.photo.attach(io: image3, filename: "sekinchancorn.jpg", content_type: "image/jpg")
 product3.save!
+
+puts "Finished"
