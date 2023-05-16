7fe6354ff2351aca0691eaefed39eb3d944d4095
wankoon puah puahwankoon@gmail.com <puahwankoon@gmail.com>
2023-05-17 00:09:20 +0800

    turbo stream figuring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,20 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_render_cart
+  before_action :initialize_cart
+
+  def set_render_cart
+    @render_cart = true
+  end
+
+  def initialize_cart
+    @cart ||= Cart.find_by(id:session[:cart_id])
+    if @cart.nil?
+      @cart = Cart.create
+      session[:cart_id] = @cart.id
+    end
+  end
 
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   validates :name, :description, :price, presence: :true
   has_one_attached :photo
   monetize :price_cents
+  has_many :orderables
+  has_many :carts, through: :orderables
 end
