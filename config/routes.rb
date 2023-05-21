5c08167689dea0185ee42e552f0555103e64c5c8
wankoon puah puahwankoon@gmail.com <puahwankoon@gmail.com>
2023-05-21 20:11:45 +0800

    homepage i18n done

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,22 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
-  root to: "pages#home"
-  devise_for :users
+    root to: "pages#home"
+    devise_for :users
 
-  get 'carts', to: 'carts#show'
-  post 'carts/add_in_product'
-  post 'carts/add_in_cart'
-  post 'carts/remove'
+    get 'carts', to: 'carts#show'
+    post 'carts/add_in_product'
+    post 'carts/add_in_cart'
+    post 'carts/remove'
 
-  resources :products
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    resources :products
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :orders, only: [:show, :create] do
-    resources :payments, only: :new
-  end
-  resources :contacts, only: [:new, :create]
-  resources :reviews, only: [:index, :create]
+    # Defines the root path route ("/")
+    # root "articles#index"
+    resources :orders, only: [:show, :create] do
+      resources :payments, only: :new
+    end
+    resources :contacts, only: [:new, :create]
+    resources :reviews, only: [:index, :create]
   end
 end
