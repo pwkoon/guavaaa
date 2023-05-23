a0bfe2023a7302c6f7d3c46d7569d53bb984bb7b
wankoon puah puahwankoon@gmail.com <puahwankoon@gmail.com>
2023-05-23 14:06:34 +0800

    done remove migrating

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,7 +1,6 @@
 class Cart < ApplicationRecord
   has_many :orderables
   has_many :products, through: :orderables
-  monetize :price_cents
 
   def total
     orderables.to_a.sum { |orderable| orderable.total }
