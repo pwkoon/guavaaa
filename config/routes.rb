2af991cbbe8343f9c6cc935f4375efb2ed382382
wankoon puah puahwankoon@gmail.com <puahwankoon@gmail.com>
2023-05-27 14:56:37 +0800

    routes change

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  mount StripeEvent::Engine, at: '/stripe-webhooks'
   scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
-    mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
     devise_for :users
     root to: "pages#home"
-    mount StripeEvent::Engine, at: '/stripe-webhooks'
     resources :products
     # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
