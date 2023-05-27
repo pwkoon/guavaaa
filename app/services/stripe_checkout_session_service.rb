233a4361214d5c79211ffc8f54a1c082a54ae58d
wankoon puah puahwankoon@gmail.com <puahwankoon@gmail.com>
2023-05-27 15:20:16 +0800

    save

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -1,4 +1,4 @@
-class StripeCheckoutSessionService
+class StripeCheckoutService
   def call(event)
     order = Order.find_by(checkout_session_id: event.data.object.id)
     order.update(state: 'paid')
