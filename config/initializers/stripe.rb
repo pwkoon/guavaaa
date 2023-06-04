c7cd6ee0bcf25182ed33d5cee5ab5dbb8af5a9dc
wankoon puah <puahwankoon@gmail.com>
2023-06-05 00:44:52 +0800

    remove stripe webhook

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -6,8 +6,8 @@ Rails.configuration.stripe = {
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
 
-StripeEvent.signing_secret = Rails.configuration.stripe[:signing_secret]
+# StripeEvent.signing_secret = Rails.configuration.stripe[:signing_secret]
 
-StripeEvent.configure do |events|
-  events.subscribe 'checkout.session.completed', StripeCheckoutSessionService.new
-end
+# StripeEvent.configure do |events|
+#   events.subscribe 'checkout.session.completed', StripeCheckoutSessionService.new
+# end
