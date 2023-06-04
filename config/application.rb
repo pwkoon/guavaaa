0f7fe4176e0bed84f87f9cb90b7b8a9020dfbd10
wankoon puah <puahwankoon@gmail.com>
2023-06-05 00:39:59 +0800

    trying resolve name error

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,7 @@ module Guavaaa
     config.load_defaults 7.0
 
     # Configuration for the application, engines, and railties goes here.
+    config.autoload_paths += %W(#{config.root}/lib)
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
